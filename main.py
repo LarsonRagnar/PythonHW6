bbe892b8a52726c70c2aeb652b4c19dafccbfa2e
LarsonRagnar <larionov1992@gmail.com>
2023-05-09 20:02:35 +0300

    6HW

---
+++ main.py
@@ -0,0 +1,59 @@
+'''
+Задача 30:  Заполните массив элементами арифметической прогрессии.
+Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+Каждое число вводится с новой строки.
+
+'''
+import random
+
+# one_number, step, quantity = int(input('Введите первое число ')), int(input('Введите шаг ')), int(
+#     input('Введите количество '))
+# print(one_number,step,quantity)
+# result=[]
+# def progression(one_number,step,quantity):
+#     if quantity!=0:
+#         result.append(one_number)
+#         one_number+=step
+#         progression(one_number,step,quantity-1)
+#     else:
+#         return print(result)
+# print(progression(one_number,step,quantity))
+
+# 2 решение которое я сделал когда посмотрел получше задачу и эталоное решение
+
+
+# one_number, step, quantity = int(input('Введите первое число ')), int(input('Введите шаг ')), int(
+#     input('Введите количество '))
+# print(one_number,step,quantity)
+# list1=[]
+# for i in range(quantity):
+#     list1.append(one_number)
+#     one_number+=step
+# print(list1)
+
+
+
+'''
+Задача 32: Определить индексы элементов массива (списка), 
+значения которых принадлежат заданному диапазону 
+(т.е. не меньше заданного минимума и не больше заданного максимума)
+
+'''
+
+
+# from random import randint
+# print(list1:=[random.randint(-10,10) for _ in range (10)])
+# beginning_range, end_range = int(input('Введите начало диапазона  ')), int(input('Введите шконец диапазона '))
+# result_list=[]
+# for i in range(len(list1)):
+#     if beginning_range < list1[i] < end_range:
+#         result_list.append(i)
+# print(result_list)
+
+
+
+
+
+
+
